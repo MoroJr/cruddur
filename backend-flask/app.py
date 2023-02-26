e3a5426ac1750de6f95b0a8b583610183d2f2130
Morosanu Dan <danutmoro@yahoo.com>
2023-02-26 13:45:53 +0000

    Added Opentelemetry & gitpod ports

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,6 +3,14 @@ from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
+# Honeycomb
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
 from services.home_activities import *
 from services.user_activities import *
 from services.create_activity import *
@@ -13,6 +21,13 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 from services.notifications import *
+
+# Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 frontend = os.getenv('FRONTEND_URL')
@@ -25,6 +40,10 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+# Honeycomb - Initialize automatic instrumentation with Flask
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
 
 @app.route("/api/message_groups", methods=['GET'])
 @cross_origin()
